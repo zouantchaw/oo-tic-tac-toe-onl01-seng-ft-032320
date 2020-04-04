c44863e9ff2ab0cf1b9b2f913a2ddc1b06324f53
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-04 22:18:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,5 +29,9 @@ WIN_COMBINATIONS = [
   def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end 
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end 
 
 end 
