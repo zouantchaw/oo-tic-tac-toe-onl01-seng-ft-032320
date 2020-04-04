1d8fe5ddc0785965ca585cedd8820bcf5bae2693
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-04 22:22:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,5 +33,17 @@ WIN_COMBINATIONS = [
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end 
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else 
+      turn 
+    end 
+  end 
 
 end 
